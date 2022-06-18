8e7a61f9c8c0ada1388c29782110820c0ab3df72
Pranav C <pranavxc@gmail.com>
2022-06-18 14:32:55 +0530

    single action

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
         const app = require('express')();
         const {Noco} = require("nocodb");
         const httpServer = app.listen(process.env.PORT || 8080);
-        // app.use(await Noco.init({}, httpServer, app));
-        app.get('/dashboard',(req,res)=> res.json({msg:'success'}))
-        app.get('/',(req,res)=> res.json({msg:'success'}))
+        app.use(await Noco.init({}, httpServer, app));
         console.log(`Visit : localhost:${process.env.PORT}/dashboard`)
     } catch(e) {
         console.log(e)
