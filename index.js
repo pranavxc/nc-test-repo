93d5001c8de14a727599cc0d6859cbb801f98335
Pranav C <pranavxc@gmail.com>
2022-06-18 12:58:59 +0530

    build simple executable
    new action for sign executable

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
         const app = require('express')();
         const {Noco} = require("nocodb");
         const httpServer = app.listen(process.env.PORT || 8080);
-        app.use(await Noco.init({}, httpServer, app));
+        // app.use(await Noco.init({}, httpServer, app));
+        app.get('/dashboard',(req,res)=> res.json({msg:'success'}))
+        app.get('/',(req,res)=> res.json({msg:'success'}))
         console.log(`Visit : localhost:${process.env.PORT}/dashboard`)
     } catch(e) {
         console.log(e)
